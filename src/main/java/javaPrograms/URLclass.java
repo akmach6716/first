340eec675493b680a8dfa6f788c911ea3fb1e3e6
akmach6716 <akmach6716@gmail.com>
2023-10-09 20:31:17 +0530

    one class

---
+++ src/main/java/javaPrograms/URLclass.java
@@ -0,0 +1,12 @@
+package javaPrograms;
+
+import com.gargoylesoftware.htmlunit.javascript.host.URL;
+
+public class URLclass {
+
+	public static void main(String[] args) {
+		URL link= new URL();
+		System.out.println(link.getAllIds());
+	}
+
+}

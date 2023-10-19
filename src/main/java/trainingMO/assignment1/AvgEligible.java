094e686fb8cf7c224add076edcba1f1e6b1b07b1
akmach6716 <akmach6716@gmail.com>
2023-10-20 00:24:41 +0530

    commit before sleep

--- src/main/java/trainingMO/assignment1/AvgEligible.java
+++ src/main/java/trainingMO/assignment1/AvgEligible.java
@@ -1,10 +1,12 @@
 package trainingMO.assignment1;
 public class AvgEligible {
 	public static void main(String[] args) {
-	average(10, 20, 30) ;
+	/*average(10, 20, 30) ;
 	average(50,150,10);
-	average(0,0,0);
-	average(20,-40,-40);
+	average(0,0,0);	
+	average(20,-40,-40);*/
+		avgvariable(20,30,40,10);
+		avgvariable(12,13,14,15);
 	}
 	public static void average(int a,int b,int c)
 	{
@@ -26,6 +28,18 @@ public class AvgEligible {
 		{
 			System.out.println(avg+" :given num is negative number");
 		}
+	}
+	//Average using vairable arguments
+	public static void avgvariable(int ...value)
+	{
+		int arglnth= value.length;
+		double sum=0;
+		for(int i=0;i<arglnth;i++)
+		{
+		sum= sum+value[i];
+		}
+		double avg=sum/arglnth;
+		System.out.println("Average is  :  "+avg);
 	}
 
 }
